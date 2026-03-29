7f0aa433cee3ed9e5ee8f97fb945f9208db0f548
Parag-T <Parag.tonpe@millenniumtechlink.com>
2026-03-29 23:42:15 +0530

    deployment changes

--- backend/build.sh
+++ backend/build.sh
@@ -7,7 +7,6 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Run database migrations using Alembic
-# Note: If you don't use alembic for migrations, you can skip this or add your custom migration script.
 if [ -f "alembic.ini" ]; then
   echo "Running database migrations..."
   alembic upgrade head
